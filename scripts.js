c1ebb8c4e04d08532ac13344656964a1fec283b4
malkhut <183023495+MalkhutHB@users.noreply.github.com>
2025-07-24 18:58:13 -0400

    fix decimal rtl behavior

--- scripts.js
+++ scripts.js
@@ -22,34 +22,38 @@ buttons.addEventListener("mousedown", (event) => {
         screenNumber += button.textContent;
     }
     else if (operation.includes(character)) {
-        if (!input1) input1 = Number(screen.textContent);
-        else input1 = operate(input1, screen.textContent, operator);
+        if (!input1) input1 = Number(screenNumber);
+        else input1 = operate(input1, screenNumber, operator);
         screen.textContent = character;
         screenNumber = "";
         operator = character;
     }
     else if (character == "=") {
-        if (input1) screen.textContent = operate(input1, screen.textContent, operator);
+        if (input1) screen.textContent = operate(input1, screenNumber, operator);
         else screen.textContent = "";
+        screenNumber = screen.textContent;
         result = Number(screen.textContent);
         input1 = "";
     }
     else if (character == "AC") {
-        screen.textContent = "";
+        screenNumber = screen.textContent = "";
         input1 = "";
     }
     else if (character == "ANS") {
-        if (operation.includes(screen.textContent)) screen.textContent = "";
-        else if (screen.textContent == "0") screen.textContent = "";
-        screen.textContent += result;
+        if (operation.includes(screen.textContent)) screenNumber = screen.textContent = "";
+        else if (screen.textContent == "0") screenNumber = screen.textContent = "";
+        screenNumber = screen.textContent += result;
     }
     else if (character == ".") {
         if (operation.includes(screen.textContent)) screen.textContent = "";
         let tempNumber = screen.textContent;
         tempNumber += "\u202A.\u202C";
         screen.textContent = tempNumber;
+        screenNumber += ".";
     }
+
     secondaryScreen.textContent = input1;
+    console.log("ScreenText: " + screen.textContent + ", ScreenNumber: " + screenNumber);
 })
 
 
